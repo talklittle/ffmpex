065ea1f4f222341786490347d05e7279e6534f4f
Andrew Shu <talklittle@gmail.com>
2016-07-27 16:25:23 -0700

    deps: ex_doc

--- mix.exs
+++ mix.exs
@@ -29,7 +29,7 @@ defmodule FFmpex.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    []
+    [{:ex_doc, ">= 0.0.0", only: :dev}]
   end
 
   defp description do

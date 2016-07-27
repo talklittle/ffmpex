19f135bef21750ba4083aef42d3e1be73e5a01fc
Andrew Shu <talklittle@gmail.com>
2016-07-27 16:22:27 -0700

    mix: description and Hex package info

--- mix.exs
+++ mix.exs
@@ -7,7 +7,9 @@ defmodule FFmpex.Mixfile do
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
-     deps: deps()]
+     deps: deps(),
+     description: description(),
+     package: package()]
   end
 
   # Configuration for the OTP application
@@ -28,5 +30,18 @@ defmodule FFmpex.Mixfile do
   # Type "mix help deps" for more examples and options
   defp deps do
     []
+  end
+
+  defp description do
+    "FFmpeg command line wrapper."
+  end
+
+  defp package do
+    [
+      files: ["lib", "mix.exs", "README*", "CHANGELOG*", "LICENSE*"],
+      maintainers: ["Andrew Shu"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/talklittle/ffmpex"}
+    ]
   end
 end

e4621c5b46c9a18175740280701c58fde3759119
Andrew Shu <talklittle@gmail.com>
2016-07-27 17:22:42 -0700

    mix: drop elixir requirement to 1.0 (was 1.3)

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule FFmpex.Mixfile do
   def project do
     [app: :ffmpex,
      version: "0.1.0",
-     elixir: "~> 1.3",
+     elixir: "~> 1.0",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps(),

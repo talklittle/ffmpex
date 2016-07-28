449f11c2412a3f8b85b4fb23d10aa8489792e5a8
Andrew Shu <talklittle@gmail.com>
2016-07-27 17:00:35 -0700

    Revert "test: increase maxrate"
    This reverts commit 2b187812e63dfa41d1a35c715bd471f92b4b8b11.

--- lib/ffmpex.ex
+++ lib/ffmpex.ex
@@ -26,7 +26,7 @@ defmodule FFmpex do
         |> add_output_file("/path/to/output.avi")
           |> add_stream_specifier(%StreamSpecifier{stream_type: :video})
             |> add_stream_option(option_b("64k"))
-          |> add_file_option(option_maxrate("256k"))
+          |> add_file_option(option_maxrate("128k"))
           |> add_file_option(option_bufsize("64k"))
 
       system_cmd_result = execute(command)

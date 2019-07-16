e657273db2e971013bc1d86240a738fb8b79cb2b
Andrew Shu <talklittle@gmail.com>
2019-07-16 15:28:17 -0700

    changelog and bump version to 0.6.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule FFmpex.Mixfile do
   def project do
     [
       app: :ffmpex,
-      version: "0.5.2",
+      version: "0.6.0",
       elixir: "~> 1.4",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
